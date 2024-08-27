737e2691b02b424af5f4ab15d5204ab08bf3be6d
Htet Wai Yan <hw.yan@mit.com.mm>
2024-08-27 10:52:07 +0700

    feat: nav components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 export default function Home() {
   return (
     <section className="w-full flex-center flex-col">
@@ -9,6 +11,8 @@ export default function Home() {
       <p className="desc text-center">
         Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
       </p>
+      
+      <Feed />
     </section>
   );
 }
